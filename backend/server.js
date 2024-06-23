cf18dc3e9ff455e1e86d03268348feaf53563c19
Ankit7rma <ankitsharma290101@gmail.com>
2024-06-23 15:10:06 +0530

    Chat App

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
 import connectToMongoDB from "./db/dbConnect.js";
 import messageRouter from "./routes/messageRouter.js";
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json()); //to parse the incoming requests from the json requests (req.body)
-
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
-app.use("/message", messageRouter);
+app.use("/api/messages", messageRouter);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
