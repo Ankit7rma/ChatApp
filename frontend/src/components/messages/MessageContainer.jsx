672c522407b1853d27cb7d146c1b0f204d56862e
Ankit7rma <ankitsharma290101@gmail.com>
2024-06-29 16:22:25 +0530

    basic frontend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -11,7 +11,7 @@ export const MessageContainer = () => {
           <NoChatSelected />
         ) : (
           <>
-            // {/* Header */}
+            // sads // {/* Header */}
             <div className="bg-slate-500 px-4 py-2 mb-2">
               <span className="label-text">To:</span>{" "}
               <span className="text-gray-900 font-bold">John doe</span>
